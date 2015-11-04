828d3d03854808c20d55e1a70d8a9a3ee4e50081
izhbannikov <zhbannikov.ilya@gmail.com>
2015-11-04 18:13:35 +0300

    lattice Example 2 added.

---
+++ lattice_example_2.R
@@ -0,0 +1,20 @@
+#---------- lattice Example #2------------#
+# http://www.statmethods.net/advgraphs/trellis.html
+
+library(lattice)
+attach(mtcars)
+
+# Customizing Lattice Graphs
+panel.smoother <- function(x, y) {
+  panel.xyplot(x, y) # show points 
+  panel.loess(x, y)  # show smoothed line 
+}
+
+hp <- cut(hp,3) # divide horse power into three bands 
+xyplot(mpg~wt|hp, scales=list(cex=.8, col="red"),
+       panel=panel.smoother,
+       xlab="Weight", ylab="Miles per Gallon", 
+       main="MGP vs Weight by Horse Power")
+
+
+#------------ End of lattice Example #2 ------------#
